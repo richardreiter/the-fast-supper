b78b9115cb181a9c9210f531dd8284939c300b4b
richardreiter <richard.reiter.dev@gmail.com>
2022-04-17 15:17:56 +0000

    Add summernote fields and test post

--- fastblog/admin.py
+++ fastblog/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
 from .models import Post
 
 """
@@ -6,5 +7,10 @@ Many thanks to Matt Rudge and CI's 'I Think Therefore I Blog'
 Walkthrough project - a great reference, inspiration and example:
 https://github.com/Code-Institute-Solutions/Django3blog
 """
-# add post model to admin panel
-admin.site.register(Post)
+
+
+# decorator to register post model and post admin class
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+    # summernote to use blog content field
+    summernote_fields = ('content')
