8c95c48b68123cc7a88417c7071fcf3e3ee3caf1
richardreiter <richard.reiter.dev@gmail.com>
2022-04-17 17:13:32 +0000

    Add configuration to admin panel

--- fastblog/admin.py
+++ fastblog/admin.py
@@ -12,5 +12,14 @@ https://github.com/Code-Institute-Solutions/Django3blog
 # decorator to register post model and post admin class
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
+
+    # display on admin panel title, slug, status, created_on
+    list_display = ('title', 'slug', 'status', 'created_on')
+    # search title or content to help find post
+    search_fields = ['title', 'content']
+    # when entering post title, slug field to be auto generated
+    prepopulated_fields = {'slug': ('title',)}
+    # box to filter posts by status or creation date
+    list_filter = ('status', 'created_on')
     # summernote to use blog content field
     summernote_fields = ('content')
