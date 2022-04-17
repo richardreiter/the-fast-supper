58754a0eeec73455a0d126697a3f72fc75ad234a
richardreiter <richard.reiter.dev@gmail.com>
2022-04-17 17:27:47 +0000

    Add CommentAdmin class and approve_comments method

--- fastblog/admin.py
+++ fastblog/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django_summernote.admin import SummernoteModelAdmin
-from .models import Post
+from .models import Post, Comment
 
 """
 Many thanks to Matt Rudge and CI's 'I Think Therefore I Blog'
@@ -23,3 +23,21 @@ class PostAdmin(SummernoteModelAdmin):
     list_filter = ('status', 'created_on')
     # summernote to use blog content field
     summernote_fields = ('content')
+
+
+# decorator to register comment
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+
+    # display on admin panel name, body, post, created_on, approved
+    list_display = ('name', 'body', 'post', 'created_on', 'approved')
+    # box to filter posts by approved or creation date
+    list_filter = ('approved', 'created_on')
+    # search name, email, body
+    search_fields = ['name', 'email', 'body']
+    # action to manually approve comments
+    actions = ['approve_comments']
+
+    # method to set approved to True
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
