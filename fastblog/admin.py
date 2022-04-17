a7646ee3ffdb1bf172ad82f2a8f5930028b588e1
richardreiter <richard.reiter.dev@gmail.com>
2022-04-17 14:39:20 +0000

    Add post model to admin panel and create superuser

--- fastblog/admin.py
+++ fastblog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+"""
+Many thanks to Matt Rudge and CI's 'I Think Therefore I Blog'
+Walkthrough project - a great reference, inspiration and example:
+https://github.com/Code-Institute-Solutions/Django3blog
+"""
+# add post model to admin panel
+admin.site.register(Post)
