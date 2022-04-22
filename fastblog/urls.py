0df1c13b61bc9aa711ff4ceaf0900eac2020903c
richardreiter <richard.reiter.dev@gmail.com>
2022-04-22 21:21:50 +0000

    Add PostLike view url in urls.py

--- fastblog/urls.py
+++ fastblog/urls.py
@@ -12,4 +12,5 @@ urlpatterns = [
     # render this class as a view
     path('', views.PostList.as_view(), name='home'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
+    path('like/<slug:slug>/', views.PostLike.as_view(), name='post_like'),
 ]
