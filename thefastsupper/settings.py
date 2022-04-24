e48e962faf5c815b341fef9b25770a854079e4f3
richardreiter <richard.reiter.dev@gmail.com>
2022-04-24 14:35:34 +0000

    Add message tags to settings.py

--- thefastsupper/settings.py
+++ thefastsupper/settings.py
@@ -10,9 +10,17 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+"""
+Many thanks to Matt Rudge and CI's 'I Think Therefore I Blog'
+Walkthrough project - a great reference, inspiration and example:
+https://github.com/Code-Institute-Solutions/Django3blog
+"""
+
 from pathlib import Path
 import os
 import dj_database_url
+# import messages to assign tags to them
+from django.contrib.messages import constants as messages
 # env.py not in production, conditional below to avoid app throwing an error.
 if os.path.isfile('env.py'):
     import env
@@ -65,6 +73,15 @@ SITE_ID = 1
 # redirect to home when login/out
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+# tags for bootstrap classes to indicate category of message
+MESSAGE_TAGS = {
+    messages.DEBUG: 'alert-info',
+    messages.INFO: 'alert-info',
+    messages.SUCCESS: 'alert-success',
+    messages.WARNING: 'alert-warning',
+    messages.ERROR: 'alert-danger',
+}
 
 # crispy to use bootstrap classes for formatting
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
